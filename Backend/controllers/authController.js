09cfedffe86d40460939cbe10d494cc3b5f1afb5
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-13 23:35:48 +0430

    login api created

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,15 +1,18 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+//Sing Up User
 export const Signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
+    
     const userExist = await User.findOne({ email });
     if (userExist) {
       return res.status(400).json({ message: "User already exist" });
     }
 
+    //Password Hash
     const hashPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashPassword });
 
@@ -18,3 +21,13 @@ export const Signup = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
+//Login User
+export const Login = async (req, res) => {
+  try {
+
+  }catch(error){
+    res.status(500).json({ message: "Server error", error: error.message });
+    
+  }
+}
