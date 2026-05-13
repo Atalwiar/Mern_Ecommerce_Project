71f51e3ce7c6ce812c4a791f8e28f3ce788db4d8
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-14 01:48:19 +0430

    get product api is created

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,5 +1,6 @@
 import Products from "../models/Product";
 
+//create product
 const createProduct = async (req, res) =>{
   try{
     const result = await Products.create(req.body);
@@ -9,6 +10,20 @@ const createProduct = async (req, res) =>{
       res.json({message : "Product not created"});
     }
   } catch(error){
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//get product
+const getProduct = async (req,res) =>{
+  try{
+    const result = await Products.find();
+   if(result){
+    return res.status(200).json({message:"Product fetched successfully",result});
+   }else{
+    res.json({message : "Product not fetched"});
+   }
+  }catch(error){
     res.status(500).json({ message: "Server error", error: error.message });
   }
 }
