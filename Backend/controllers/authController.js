0d2e15f2563e6c6e86d3d06b320b66fd88d44d64
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-13 23:50:58 +0430

    json web token created for login

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -25,7 +25,28 @@ export const Signup = async (req, res) => {
 //Login User
 export const Login = async (req, res) => {
   try {
-
+    const {email , password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({message:"User not found"});
+      // res.redirect("/api/auth/signup");
+    }
+    const match = await bcrypt.compare(password,user.password);
+    if(!match){
+      return res.status(400).json({message:"invalid credentials"});
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.json({
+      message:"Login successful",
+      token,
+      user:{
+        id:user._id,
+        name:user.name,
+        email:user.email
+      }
+    })
   }catch(error){
     res.status(500).json({ message: "Server error", error: error.message });
     
