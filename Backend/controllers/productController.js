7e82c0879b5602dfb8e6ab0a222e5b8bacee493d
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-14 01:54:07 +0430

    update api route is created

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -7,7 +7,7 @@ const createProduct = async (req, res) =>{
     if(result){
       return res.status(201).json({message:"Product created successfully",result});
     }else{
-      res.json({message : "Product not created"});
+      res.json({message : "Product not created", result});
     }
   } catch(error){
     res.status(500).json({ message: "Server error", error: error.message });
@@ -17,13 +17,29 @@ const createProduct = async (req, res) =>{
 //get product
 const getProduct = async (req,res) =>{
   try{
-    const result = await Products.find();
+    const result = await Products.find().sort({createdAt:-1});
    if(result){
     return res.status(200).json({message:"Product fetched successfully",result});
    }else{
-    res.json({message : "Product not fetched"});
+    res.json({message : "Product not fetched", result});
    }
   }catch(error){
     res.status(500).json({ message: "Server error", error: error.message });
   }
+}
+
+
+//update product
+
+const updateProduct = async (req,res) =>{
+  try{
+    const result = await Products.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    if(result){
+      return res.status(200).json({message:"Product updated successfully",result});
+    }else{
+      res.json({message : "Product not updated",result});
+    }
+  }catch(error){
+    res.status(500).json({ message: "Server error", error: error.message });
+  }  
 }
