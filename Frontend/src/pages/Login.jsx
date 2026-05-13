804aa7d799d63b54feb490144cd4181961075454
Atalwiar077 <Atalwiar145@gmail.com>
2026-05-14 00:29:21 +0430

    Login page created

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { useState } from "react"
+import { useNavigate } from "react-router"
 
 function Login() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [message,setMessage] = useState("");
+
   return (
     <div>
       Login
